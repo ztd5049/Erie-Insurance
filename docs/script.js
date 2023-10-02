09752125fe049f9d7846c3cce8f46f52486ec6cc
ztd5049 <ztd5049@psu.edu>
2023-10-02 03:57:36 -0400

    Adding Updated website and css. Not Final

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navList = document.querySelector("#nav-list");
-
-    menuToggle.addEventListener("click", function() {
-        navList.classList.toggle("active");
-    });
-});
